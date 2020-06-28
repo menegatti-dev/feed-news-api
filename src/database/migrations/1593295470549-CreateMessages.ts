5026349ac8ebfa08e30b2e6eede2cfcdbadb78c7
Marlon Menegatti <contato@marlondev.me>
2020-06-28 02:45:34 -0300

    Migration for Message in database postgres with uuid_generate_v4()

---
+++ src/database/migrations/1593295470549-CreateMessages.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateMessages1593295470549 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'messages',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'origin',
+            type: 'varchar',
+          },
+          {
+            name: 'destiny',
+            type: 'varchar',
+          },
+          {
+            name: 'message',
+            type: 'text',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('messages');
+  }
+}
